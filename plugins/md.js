babacea65b71b99b20df000bd81d32d0c6cb58ab
cyy <cyy0418@qq.com>
2018-07-16 14:24:18 +0800

    update

--- plugins/md.js
+++ plugins/md.js
@@ -1,80 +1,80 @@
-const hljs = require("highlight.js");
-const markdown = require("markdown-it")({
+const hljs = require('highlight.js')
+const markdown = require('markdown-it')({
   html: true, // Enable HTML tags in source
   xhtmlOut: true, // Use '/' to close single tags (<br />).
   breaks: true, // Convert '\n' in paragraphs into <br>
-  langPrefix: "language-", // CSS language prefix for fenced blocks. Can be
+  langPrefix: 'language-', // CSS language prefix for fenced blocks. Can be
   linkify: false, // 自动识别url
   typographer: true,
-  quotes: "“”‘’",
+  quotes: '“”‘’',
   highlight: function(str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
         return (
           `<pre class="language-${lang}"><code>` +
           hljs.highlight(lang, str, true).value +
-          "</code></pre>"
-        );
+          '</code></pre>'
+        )
       } catch (__) {}
     }
 
     return (
       '<pre class="language"><code>' +
       markdown.utils.escapeHtml(str) +
-      "</code></pre>"
-    );
+      '</code></pre>'
+    )
   }
-});
+})
 // 表情
-var emoji = require("markdown-it-emoji");
+var emoji = require('markdown-it-emoji')
 // 下标
-var sub = require("markdown-it-sub");
+var sub = require('markdown-it-sub')
 // 上标
-var sup = require("markdown-it-sup");
+var sup = require('markdown-it-sup')
 // <dl/>
-var deflist = require("markdown-it-deflist");
+var deflist = require('markdown-it-deflist')
 // <abbr/>
-var abbr = require("markdown-it-abbr");
+var abbr = require('markdown-it-abbr')
 // footnote
-var footnote = require("markdown-it-footnote");
+var footnote = require('markdown-it-footnote')
 // insert 带有下划线 样式 ++ ++
-var insert = require("markdown-it-ins");
+var insert = require('markdown-it-ins')
 // mark
-var mark = require("markdown-it-mark");
+var mark = require('markdown-it-mark')
 // taskLists
-var taskLists = require("markdown-it-task-lists");
+var taskLists = require('markdown-it-task-lists')
 // container
-var container = require("markdown-it-container");
+var container = require('markdown-it-container')
 //
-var toc = require("markdown-it-toc");
+var toc = require('markdown-it-toc')
 // add target="_blank" to all link
 var defaultRender =
   markdown.renderer.rules.link_open ||
   function(tokens, idx, options, env, self) {
-    return self.renderToken(tokens, idx, options);
-  };
+    return self.renderToken(tokens, idx, options)
+  }
 markdown.renderer.rules.link_open = function(tokens, idx, options, env, self) {
   // If you are sure other plugins can't add `target` - drop check below
-  var aIndex = tokens[idx].attrIndex("target");
+  var aIndex = tokens[idx].attrIndex('target')
 
   if (aIndex < 0) {
-    tokens[idx].attrPush(["target", "_blank"]); // add new attribute
+    tokens[idx].attrPush(['target', '_blank']) // add new attribute
   } else {
-    tokens[idx].attrs[aIndex][1] = "_blank"; // replace value of existing attr
+    tokens[idx].attrs[aIndex][1] = '_blank' // replace value of existing attr
   }
 
   // pass token to default renderer.
-  return defaultRender(tokens, idx, options, env, self);
-};
+  return defaultRender(tokens, idx, options, env, self)
+}
 // math katex
 markdown
   .use(emoji)
   .use(sup)
   .use(sub)
   .use(container)
-  .use(container, "hljs-left") /* align left */
-  .use(container, "hljs-center") /* align center */
-  .use(container, "hljs-right") /* align right */
+  .use(container, 'hljs-left') /* align left */
+  .use(container, 'hljs-center') /* align center */
+  .use(container, 'hljs-right') /* align right */
   .use(deflist)
   .use(abbr)
   .use(footnote)
@@ -82,6 +82,6 @@ markdown
   .use(mark)
   .use(container)
   .use(taskLists)
-  .use(toc);
+  .use(toc)
 
-export default markdown;
+export default markdown
