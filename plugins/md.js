c8120f7822f909f90a7f00da871aee0dda66e71b
cyy <cyy0418@qq.com>
2018-08-03 09:57:09 +0800

    update

--- plugins/md.js
+++ plugins/md.js
@@ -11,7 +11,7 @@ const markdown = require('markdown-it')({
     if (lang && hljs.getLanguage(lang)) {
       try {
         return (
-          `<pre class="language-${lang}"><code>` +
+          `<pre class="language-${lang}"><svg style="margin-top: 10px; margin-left: 1rem;" xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5"></circle><circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5"></circle><circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5"></circle></g></svg><code>` +
           hljs.highlight(lang, str, true).value +
           '</code></pre>'
         )
@@ -19,7 +19,7 @@ const markdown = require('markdown-it')({
     }
 
     return (
-      '<pre class="language"><code>' +
+      '<pre class="language"><svg style="margin-top: 10px; margin-left: 1rem;" xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5"></circle><circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5"></circle><circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5"></circle></g></svg><code>' +
       markdown.utils.escapeHtml(str) +
       '</code></pre>'
     )
