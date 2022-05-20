2ea25fab8f7c65aa2aa26e4bea9b61c69a78f2a5
cuiyuanyuan <cuiyuanyuan@uniwyse.com>
2022-05-20 12:15:03 +0800

    feat:增加友情链接

--- components/footer.vue
+++ components/footer.vue
@@ -3,10 +3,14 @@
     <a href="https://github.com/cyyjs" target="_blank">
       <img src="~assets/img/github.png" alt="" class="github">
     </a>
-    Copyright © 2018-present Cyyjs
+    Copyright © 2018-present <a href="mailto:cyyjs@cyyjs.top">Cyyjs</a>
+    <div class="link">
+      <label for="">友情链接:</label>
+      <a href="https://kalacloud.com" target="_blank">卡拉云后台系统</a>
+    </div>
   </footer>
 </template>
-<style scoped>
+<style lang="scss" scoped>
 footer {
     padding: 3.5rem 0;
     text-align: center;
@@ -14,6 +18,17 @@ footer {
     margin: auto;
     margin-top: 10px;
     background-color: #fff;
+}
+.link {
+  margin-top: 10px;
+  font-size: 14px;
+  label {
+    font-weight: bold;
+  }
+  a {
+    color: #666;
+
+  }
 }
 .github {
     vertical-align: middle;
