4074d7f5656ab8188f1efdaac8d56f268c04ce69
cuiyuanyuan <cuiyuanyuan@uniwyse.com>
2022-05-23 19:31:27 +0800

    feat: 增加友链

--- components/footer.vue
+++ components/footer.vue
@@ -6,10 +6,25 @@
     Copyright © 2018-present <a href="mailto:cyyjs@cyyjs.top">Cyyjs</a>
     <div class="link">
       <label for="">友情链接:</label>
-      <a href="https://kalacloud.com" target="_blank">卡拉云后台系统</a>
+      <a v-for="item in links" :key="item.url" :href="item.url" target="_blank">{{item.name}}</a>
     </div>
   </footer>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      links: [{
+        name: '卡拉云后台系统',
+        url: 'https://kalacloud.com'
+      }, {
+        name: '东隅已逝|桑榆非晚',
+        url: 'https://h1z3y3.me/'
+      }]
+    }
+  }
+}
+</script>
 <style lang="scss" scoped>
 footer {
     padding: 3.5rem 0;
@@ -27,7 +42,16 @@ footer {
   }
   a {
     color: #666;
-
+    &::after {
+      content: '|';
+      margin: 0 10px;
+    }
+    &:last-child::after {
+      content: '';
+    }
+    &:hover {
+      color: #46bd87;
+    }
   }
 }
 .github {
